231fd1b63b838e639b3b20359ac73578338d3d73
melokb2023 <kyle.melo@lccdo.edu.ph>
2023-11-09 22:10:55 +0800

    initial2

--- admin/adminconfig.php
+++ admin/adminconfig.php
@@ -1,32 +1,108 @@
-<?php
-$sql = "SELECT * FROM contacts";
-$result = mysqli_query($conn, $sql);
-$i = 1;
-if (mysqli_num_rows($result) > 0) {
-?>
-<table class="table table-bordered table-hover table-striped">
-	<tr>
-		<th>Complete Name</th>
-		<th>Email</th>
-		<th>Category</th>
-		<th>Message</th>
-	</tr>
-	<?php
-	while ($row = mysqli_fetch_assoc($result)) {		
-	?>
-	<tr>
-		<td><?php echo $i; ?></td>
-		<td><?php echo $row['firstname']; ?></td>
-        <td><?php echo $row['lastname']; ?></td>
-		<td><?php echo $row['email']; ?></td>
-        <td><?php echo $row['category']; ?></td>
-		<td><?php echo $row['message']; ?></td>
-	</tr>
-	<?php
-    $i++;
-	}
-	?>
-</table>
-<?php
-}
-?>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Funda Of Web IT</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-7">
+
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search data">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+                                <form action="" method="GET">
+                                <div class="row">
+                                <div class="col-md-4">
+                                <select name ="category" class="form-select">
+                                  <option value="">Select Status: </option>
+                                  <option value="Product Info" <? isset($_GET['category']) == true ? ($_GET['category'] =='Product Info' ? 'selected':'') :''?>> Product Info</option>
+                                  <option value="Complain" <? isset($_GET['category']) == true ? ($_GET['category'] =='Complain' ? 'selected':'') :''?>> Complain </option>
+                                  </select>
+                                    </select>
+
+</div>
+</div>
+</form>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+
+
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Complete Name</th>
+                                    <th>Email</th>
+                                    <th>Date and Time</th>
+                                    <th>Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $con = mysqli_connect("localhost","root","","contactform");
+
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM users WHERE CONCAT(firstname,lastname,email) LIKE '%$filtervalues%' ";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['id']; ?></td>
+                                                    <td><?= $items['firstname']; ?><?= $items['lastname']; ?></td>
+                                                    <td><?= $items['dateandtime']; ?></td>
+                                                    <td><?= $items['email']; ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">No Record Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
